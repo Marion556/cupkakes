35aafb6fe6e0eda2aadba435fe5bad77d581463e
Marion <71854416+Marion556@users.noreply.github.com>
2021-04-18 18:28:03 -0400

    first commit

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField
+from wtforms.validators import InputRequired, Optional
+
+
+class AddCupcakeForm(FlaskForm):
+
+    flavor = StringField("Cupcake Flavor", validators=[InputRequired()])
+    size = StringField("Size", validators=[InputRequired()])
+    rating = FloatField("Rating")
+    image = StringField("Add Cupcake URL", validators=[Optional()])
